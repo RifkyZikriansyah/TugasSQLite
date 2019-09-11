43ea2263411e120044ace57bd636c05a9126845b
Rifky Zikriansyah <zikriansyah2002@gmail.com>
2019-09-11 11:14:40 +0700

    select alertdiaolog

--- app/src/main/java/com/example/tugassqlite/Main3Activity.java
+++ app/src/main/java/com/example/tugassqlite/Main3Activity.java
@@ -9,19 +9,20 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 public class Main3Activity extends AppCompatActivity {
-
+    LinearLayout opsi;
     RecyclerView recyclerview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
-
+        opsi = findViewById(R.id.opsi);
         recyclerview = (RecyclerView) findViewById(R.id.recyclerview);
-        recyclerview.setOnClickListener(new View.OnClickListener() {
+        opsi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
                 showDialog();
